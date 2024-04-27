3cd0019ba58241cb7c2351a0174d2bc3243cac4e
haffe017 <haffe017@umn.edu>
2024-04-27 14:51:57 -0500

    Squashed commit of the following:
    commit 765962ec1630251b433dead791398e2ea8117924
    Author: haffe017 <haffe017@umn.edu>
    Date:   Sat Apr 27 14:46:09 2024 -0500
    
        Videos page fully implemented
    
    commit 6636e1997fa012846227c4a608ee7d3f6590be9c
    Author: haffe017 <haffe017@umn.edu>
    Date:   Fri Apr 26 20:55:13 2024 -0500
    
        made the videos page work
    
        DO NOT! I REPEAT DO NOT GO FULL SCREEN!
    
    commit b722ea8c8fc1d839bfadb82abf334363e7356c93
    Author: haffe017 <haffe017@umn.edu>
    Date:   Fri Apr 26 15:45:19 2024 -0500
    
        Dark mode now works

--- lib/app_ui.dart
+++ lib/app_ui.dart
@@ -5,6 +5,8 @@ import 'package:final_project/goals.dart';
 import 'package:final_project/settings.dart';
 import 'package:final_project/scheduler.dart';
 import 'Home.dart';
+import 'videos.dart';
+import 'package:provider/provider.dart';
 
 class app_ui extends StatefulWidget {
   const app_ui({Key? key}) : super(key: key);
@@ -14,15 +16,16 @@ class app_ui extends StatefulWidget {
 }
 
 class _app_uiState extends State<app_ui> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 2;
 
   final List<Widget> _pages = [
-    const Home(),
     const Scheduler(),
     const Goals(),
-    const videos(),
+    const Home(),
+    const Videos(),
     const Settings(),
   ];
+
 
   @override
   Widget build(BuildContext context) {
@@ -34,44 +37,47 @@ class _app_uiState extends State<app_ui> {
         theme.bottomNavigationBarTheme.unselectedItemColor ?? Colors.grey;
     final Color selectedItemColor =
         theme.bottomNavigationBarTheme.selectedItemColor ?? Colors.blue;
-
+    bool thingy = context.watch<UserProvider>().isFullScreen;
     return Scaffold(
       body: _pages[_selectedIndex],
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(0.0),
-        child: GNav(
-          backgroundColor: backgroundColor,
-          color: unselectedItemColor,
-          activeColor: selectedItemColor,
-          gap: 8,
-          selectedIndex: _selectedIndex,
-          onTabChange: (index) {
-            setState(() {
-              _selectedIndex = index;
-            });
-          },
-          tabs: const [
-            GButton(
-              icon: Icons.home,
-              text: 'Home',
-            ),
-            GButton(
-              icon: Icons.calendar_month,
-              text: 'Schedule',
-            ),
-            GButton(
-              icon: Icons.adjust,
-              text: 'Goals',
-            ),
-            GButton(
-              icon: Icons.ondemand_video_sharp,
-              text: 'Videos',
-            ),
-            GButton(
-              icon: Icons.settings,
-              text: 'Settings',
-            ),
-          ],
+      bottomNavigationBar: Visibility(
+        visible: !thingy, // Show bottom navigation bar if isFullScreen is false
+        child: Padding(
+          padding: const EdgeInsets.all(0.0),
+          child: GNav(
+            backgroundColor: backgroundColor,
+            color: unselectedItemColor,
+            activeColor: selectedItemColor,
+            gap: 8,
+            selectedIndex: _selectedIndex,
+            onTabChange: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            tabs: const [
+              GButton(
+                icon: Icons.calendar_month,
+                text: 'Schedule',
+              ),
+              GButton(
+                icon: Icons.adjust,
+                text: 'Goals',
+              ),
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: Icons.ondemand_video_sharp,
+                text: 'Videos',
+              ),
+              GButton(
+                icon: Icons.settings,
+                text: 'Settings',
+              ),
+            ],
+          ),
         ),
       ),
     );
